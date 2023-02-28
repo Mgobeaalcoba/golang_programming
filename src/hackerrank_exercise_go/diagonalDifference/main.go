1638b979cf4795f752c3cd6a4830b27a324fc808
Mariano Daniel Gobea Alcoba <gobeamariano@gmail.com>
2023-02-28 12:18:00 -0300

    :sparkles: With more hackerrank exercise v2.0 28.2.2023

---
+++ src/hackerrank_exercise_go/diagonalDifference/main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'diagonalDifference' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+func diagonalDifference(arr [][]int32) int32 {
+    // Write your code here
+    var diagonal1 int32
+    var diagonal2 int32
+    
+    for i := 0; i < len(arr[0]); i++ {
+        for j := 0; j < len(arr[0]); j++ {
+            if i == j {
+                diagonal1 += arr[i][j]
+            }
+            if (len(arr[0])-1-i) == j {
+                diagonal2 += arr[i][j]
+            }
+        }
+    } 
+    
+    var difference int32 = diagonal1 - diagonal2
+    return int32(math.Abs(float64(difference)))
+}
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 16 * 1024 * 1024)
+
+    stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+    checkError(err)
+
+    defer stdout.Close()
+
+    writer := bufio.NewWriterSize(stdout, 16 * 1024 * 1024)
+
+    nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+    checkError(err)
+    n := int32(nTemp)
+
+    var arr [][]int32
+    for i := 0; i < int(n); i++ {
+        arrRowTemp := strings.Split(strings.TrimRight(readLine(reader)," \t\r\n"), " ")
+
+        var arrRow []int32
+        for _, arrRowItem := range arrRowTemp {
+            arrItemTemp, err := strconv.ParseInt(arrRowItem, 10, 64)
+            checkError(err)
+            arrItem := int32(arrItemTemp)
+            arrRow = append(arrRow, arrItem)
+        }
+
+        if len(arrRow) != int(n) {
+            panic("Bad input")
+        }
+
+        arr = append(arr, arrRow)
+    }
+
+    result := diagonalDifference(arr)
+
+    fmt.Fprintf(writer, "%d\n", result)
+
+    writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
