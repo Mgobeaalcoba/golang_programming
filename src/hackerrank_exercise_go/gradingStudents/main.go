92d1026828e962d675a003f48f4c5315231bdd81
Mariano Gobea Alcoba <gobeamariano@gmail.com>
2023-03-14 13:43:03 -0300

    :sparkles: With more exercise 2.0 14.3.2023

---
+++ src/hackerrank_exercise_go/gradingStudents/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'gradingStudents' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY grades as parameter.
+ */
+
+func gradingStudents(grades []int32) []int32 {
+
+	roundGrades := []int32{}
+
+	for _, grade := range grades {
+		if grade < 38 {
+			roundGrades = append(roundGrades, grade)
+		} else {
+			numbCompare := (grade / 5) + 1
+			if (numbCompare*5)-grade >= 3 {
+				roundGrades = append(roundGrades, grade)
+			} else {
+				roundGrades = append(roundGrades, numbCompare*5)
+			}
+		}
+	}
+
+	return roundGrades
+
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	gradesCount, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+
+	var grades []int32
+
+	for i := 0; i < int(gradesCount); i++ {
+		gradesItemTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+		checkError(err)
+		gradesItem := int32(gradesItemTemp)
+		grades = append(grades, gradesItem)
+	}
+
+	result := gradingStudents(grades)
+
+	for i, resultItem := range result {
+		fmt.Fprintf(writer, "%d", resultItem)
+
+		if i != len(result)-1 {
+			fmt.Fprintf(writer, "\n")
+		}
+	}
+
+	fmt.Fprintf(writer, "\n")
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
