3d325515a087492ac82e1823c06f3ca1543feb46
Mariano Gobea Alcoba <gobeamariano@gmail.com>
2023-03-18 15:37:58 -0300

    :sparkles: With API-REST Module at 80%

---
+++ src/goweb/05-api-rest/go.mod
@@ -0,0 +1,15 @@
+module apirest
+
+go 1.20
+
+require (
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/howeyc/fsnotify v0.9.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/pilu/config v0.0.0-20131214182432-3eb99e6c0b9a // indirect
+	github.com/pilu/fresh v0.0.0-20190826141211-0fa698148017 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ src/goweb/05-api-rest/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"apirest/handlers"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	// RUTAS:
+
+	mux := mux.NewRouter() // Metodo de gorilla/mux
+
+	// ENDPOINTS:
+
+	// EndPoint "GET" para obtener info de usuarios:
+
+	mux.HandleFunc("/api/user/", handlers.GetUsers).Methods("GET")           // Ruta para obtener todos los usuarios.
+	mux.HandleFunc("/api/user/{id:[0-9]+}", handlers.GetUser).Methods("GET") // Ruta para obtener solo un usuario.
+
+	// EndPoint "POST" para agregar info de usuarios:
+
+	mux.HandleFunc("/api/user/", handlers.CreateUser).Methods("POST") // Ruta para registrar un nuevo usuario.
+
+	// EndPoint "PUT" para editar info de usuarios:
+
+	mux.HandleFunc("/api/user/{id:[0-9]+}", handlers.UpdateUser).Methods("PUT") //
+
+	// EndPoint "DELETE" para eliminar info de usuarios:
+
+	mux.HandleFunc("/api/user/{id:[0-9]+}", handlers.DeleteUser).Methods("DELETE") //
+
+	// SERVIDOR:
+
+	log.Fatal(http.ListenAndServe("localhost:5007", mux))
+
+}
+
+// Para crear los handlers de cada uno de los endpoints voy a armarlos en una carpeta sobre la raiz llamada "handlers" con un file dentro de ella llamado "handlers.go"
